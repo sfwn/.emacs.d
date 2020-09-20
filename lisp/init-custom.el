873ae0b543b4ee58152810328264d285f318b0da
sfwn <sfwnlin@gmail.com>
2020-09-20 21:39:37 +0800

    set find-function-C-source-directory

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -4,6 +4,8 @@
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
+(setq-default find-function-C-source-directory '(expand-file-name "sources/27.1" user-emacs-directory))
+
 (provide 'init-custom)
 
 ;;; init-custom.el ends here
