0b997566e7a17f9911a98a7b26ce647d9febb2b7
sfwn <sfwnlin@gmail.com>
2020-10-09 00:22:11 +0800

    init-custom.el: get-action-agent-md5

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -6,6 +6,15 @@
 
 (setq-default find-function-C-source-directory '(expand-file-name "sources/27.1" user-emacs-directory))
 
+;; get md5 of action agent
+(defun get-action-agent-md5 (image)
+  "get MD5 of action-agent"
+  (interactive "simage: ")
+  (kill-new
+   (message
+	(substring
+	 (shell-command-to-string (concat "docker run --rm --entrypoint cat " image " /opt/action/action-agent-md5")) 0 -1))))
+
 (provide 'init-custom)
 
 ;;; init-custom.el ends here
