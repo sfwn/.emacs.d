7fa1c61262e65385086d723fdaee3112996bcf93
sfwn <sfwnlin@gmail.com>
2020-10-08 19:06:49 +0800

    feature: auto-save => super-save

--- lisp/init-auto-save.el
+++ lisp/init-auto-save.el
@@ -2,22 +2,29 @@
 ;;; Commentary:
 ;;; Code:
 
-(add-to-list 'load-path (expand-file-name "github-plugins/auto-save" user-emacs-directory))
-(require 'auto-save)
+;; (add-to-list 'load-path (expand-file-name "github-plugins/auto-save" user-emacs-directory))
+;; (require 'auto-save)
 
-(setq auto-save-silent nil)				; quietly save
-(setq auto-save-delete-trailing-whitespace t) ; automatically delete spaces at the end of the line when saving
-(setq auto-save-idle 10)
+;; (setq auto-save-silent nil)				; quietly save
+;; (setq auto-save-delete-trailing-whitespace t) ; automatically delete spaces at the end of the line when saving
+;; (setq auto-save-idle 10)
 
-;;; custom predicates if you don't want auto save.
-;;; disable auto save mode when current filetype is an gpg file.
-(setq auto-save-disable-predicates
-      '((lambda ()
-      (string-suffix-p
-      "gpg"
-      (file-name-extension (buffer-name)) t))))
+;; ;;; custom predicates if you don't want auto save.
+;; ;;; disable auto save mode when current filetype is an gpg file.
+;; (setq auto-save-disable-predicates
+;;       '((lambda ()
+;;       (string-suffix-p
+;;       "gpg"
+;;       (file-name-extension (buffer-name)) t))))
 
-(auto-save-enable)
+;; (auto-save-enable)
+
+;; super-save
+(use-package super-save
+  :ensure t
+  :config
+  (super-save-mode t)
+  (setq super-save-auto-save-when-idle t))
 
 (provide 'init-auto-save)
 
