50b9cfd86cb1c80c89c1e4133633e51afb16bcda
sfwn <sfwnlin@gmail.com>
2020-10-11 13:37:26 +0800

    feature: polish http-proxy

--- lisp/init-proxy.el
+++ lisp/init-proxy.el
@@ -2,10 +2,37 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq url-proxy-services
-      '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
-        ("http" . "0.0.0.0:1087")
-             ("https" . "0.0.0.0:1087")))
+;; Network Proxy
+
+(defconst sfwn/http_proxy "0.0.0.0:1087" "HTTP Proxy")
+
+(defun sfwn/proxy-http-show ()
+  "Show HTTP/HTTPS proxy."
+  (interactive)
+  (if url-proxy-services (message "Current HTTP Proxy is `%s'" sfwn/http_proxy)
+	(message "No HTTP Proxy")))
+
+(defun sfwn/proxy-http-enable ()
+  "Enable HTTP/HTTPS proxy."
+  (interactive)
+  (setq url-proxy-services
+		`(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
+		  ("http" . ,sfwn/http_proxy)
+		  ("https" . ,sfwn/http_proxy)))
+  (proxy-http-show))
+
+(defun sfwn/proxy-http-disable ()
+  "Disbale HTTP/HTTPS proxy."
+  (interactive)
+  (setq url-proxy-services nil)
+  (proxy-http-show))
+
+(defun sfwn/proxy-http-toggle ()
+  "Toggle HTTP/HTTPS proxy."
+  (interactive)
+  (if (bound-and-true-p url-proxy-services)
+	  (sfwn/proxy-http-disable)
+	(sfwn/proxy-http-enable)))
 
 (provide 'init-proxy)
 
