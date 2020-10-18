9e389bfedeac9001cad65bdf7a11a2c680adc01b
sfwn <sfwnlin@gmail.com>
2020-10-18 18:40:08 +0800

    init-go: add go-run & go-test & go-fill-struct

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -6,7 +6,9 @@
 (use-package go-mode
   :ensure t
   :config
-  (define-key go-mode-map (kbd "C-4") 'go-goto-function-name))
+  (define-key go-mode-map (kbd "C-4") 'go-goto-function-name)
+  (define-key go-mode-map (kbd "C-5") 'go-run)
+  (define-key go-mode-map (kbd "C-6") 'go-test-current-test))
 
 ;; Set up before-save hooks to format buffer and add/delete imports.
 ;; Make sure you don't have other gofmt/goimports hooks enabled.
@@ -28,6 +30,10 @@
   :ensure t
   :config
   (setq go-tag-args (list "-transform" "camelcase")))
+
+;; go-fill-struct
+(use-package go-fill-struct
+  :ensure t)
 
 (provide 'init-go)
 
