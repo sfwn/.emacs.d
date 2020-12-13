0c0fbe57999a61bc29887fa3364c5baf045f2ce9
sfwn <sfwnlin@gmail.com>
2020-12-13 18:00:36 +0800

    remove unused eshell & aweshell

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -2,14 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-;; eshell
-(add-hook 'eshell-exit-hook (lambda() (global-company-mode 1) (company-mode 1) (company-box-mode 1) (evil-mode 1)) 'append)
-(add-hook 'eshell-mode-hook (lambda() (global-company-mode -1) (company-mode -1) (company-box-mode -1) (evil-mode -1)) 'append)
-
-;; aweshell
-(add-to-list 'load-path (expand-file-name "github-plugins/aweshell" user-emacs-directory))
-(require 'aweshell)
-
 ;; vterm
 (use-package vterm
   :ensure t)
