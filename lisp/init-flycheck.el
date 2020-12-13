4e75dbeaeb7b77a1de672846c5b6e8d6b3ed70fd
sfwn <sfwnlin@gmail.com>
2020-12-13 17:58:57 +0800

    feature: update flycheck

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -2,25 +2,35 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq-default flycheck-emacs-lisp-load-path 'inherit)
-
-;; flycheck
 (use-package flycheck
-  :ensure t
-  :config ;; (global-flycheck-mode)
-          (add-to-list 'display-buffer-alist
-					   `(,(rx bos "*Flycheck errors*" eos)
-						 (display-buffer-reuse-window
-						  display-buffer-in-side-window)
-						 (side . bottom)
-						 (reusable-frams .visible)
-						 (window-height . 0.2)))
-)
+  :diminish
+  :commands flycheck-redefine-standard-error-levels
+  :hook (after-init . global-flycheck-mode)
+  :init (setq flycheck-global-modes
+              '(not text-mode outline-mode fundamental-mode lisp-interaction-mode
+                    org-mode diff-mode shell-mode eshell-mode term-mode vterm-mode)
+              flycheck-emacs-lisp-load-path 'inherit
+              flycheck-indication-mode (if (display-graphic-p)
+                                           'right-fringe
+                                         'right-margin)
+              ;; Only check while saving and opening files
+              flycheck-check-syntax-automatically '(save mode-enabled))
+  :config
+  ;; Prettify indication styles
+  (when (fboundp 'define-fringe-bitmap)
+    (define-fringe-bitmap 'flycheck-fringe-bitmap-arrow
+      [16 48 112 240 112 48 16] nil nil 'center))
+  (flycheck-redefine-standard-error-levels "⏴" 'flycheck-fringe-bitmap-arrow)
 
-;; flycheck-golangci-lint
-(use-package flycheck-golangci-lint
-  :ensure t
-  :hook (go-mode . flycheck-golangci-lint-setup))
+  ;; Display Flycheck errors in GUI tooltips
+  (if (display-graphic-p)
+      (use-package flycheck-posframe
+		:hook (flycheck-mode . flycheck-posframe-mode)
+        :init (setq flycheck-posframe-inhibit-functions
+					'((lambda (&rest _) (bound-and-true-p company-backend)))))
+
+    (use-package flycheck-popup-tip
+      :hook (flycheck-mode . flycheck-popup-tip-mode))))
 
 (provide 'init-flycheck)
 
